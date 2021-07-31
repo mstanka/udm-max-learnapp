8e01d364e825637c8369dee75d59196b5e4ed14e
mstanka <mice78.mm@gmail.com>
2021-07-31 13:55:10 +0200

    add header component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
+  <the-header :title="title"></the-header>
   <stored-resources :resources="storedResources"></stored-resources>
 </template>
 
 <script>
 import StoredResources from './components/learning-resources/StoredResources.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
 
 export default {
   components: {
     StoredResources,
+    TheHeader,
   },
   data() {
     return {
+      title: 'Learning Resources',
       storedResources: [
         {
           id: 'official-guide',
