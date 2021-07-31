f23b4bdc35ab8a70baf5d33d08a6037c12a381f8
mstanka <mice78.mm@gmail.com>
2021-07-31 13:14:28 +0200

    add learning-resource component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,41 @@
 <template>
-  <div>App</div>
+  <ul>
+    <learning-resource
+      v-for="res in storedResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resource>
+  </ul>
 </template>
 
 <script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
 
+export default {
+  components: {
+    LearningResource,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The Official Vue JS documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'The Google documentation',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
